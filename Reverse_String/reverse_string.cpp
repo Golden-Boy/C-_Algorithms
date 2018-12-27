b64adb43cfc8dcf6c2b3912c90185ad7379f7956
golden-boy <farbeyondaverage@gmail.com>
2018-12-27 10:47:38 -0800

    added factorial function

---
+++ Reverse_String/reverse_string.cpp
@@ -0,0 +1,20 @@
+/*
+ * Reversing a string
+ */
+
+
+
+#include <iostream>
+using namespace std;
+
+void reverseString(string) {
+	for (var = 0; ) {
+		return char[::-1];
+	}
+}
+
+int main(){
+	string >> cin;
+	reverseString (string);
+
+	return 0;
